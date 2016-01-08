9e9d82ebfa249e19605688220faa7e23094b65bf
Oskar Gewalli <gewalli@gmail.com>
2016-01-08 07:09:11 +0100

    rakefile

---
+++ rakefile.rb
@@ -0,0 +1,58 @@
+require 'albacore'
+require 'nuget_helper'
+
+$dir = File.dirname(__FILE__)
+$sln = File.join($dir, "Pollock.sln")
+
+desc "Install missing NuGet packages."
+task :restore do
+  NugetHelper.exec("restore #{$sln}")
+end
+
+desc "build"
+build :build => [:restore] do |msb|
+  msb.prop :configuration, :Debug
+  msb.prop :platform, "Any CPU"
+  msb.target = :Rebuild
+  msb.be_quiet
+  msb.nologo
+  msb.sln = $sln
+end
+
+task :default => ['build']
+
+desc "test using nunit console"
+test_runner :test => [:build] do |nunit|
+  nunit.exe = NugetHelper.nunit_path
+  files = Dir.glob(File.join($dir, "*Tests", "**", "bin", "Debug", "*Tests.dll"))
+  nunit.files = files 
+end
+
+desc "create the nuget packages"
+task :pack => [:build, :pollock_pack, :pollock_json_pack]
+
+task :pollock_pack => [:build] do |nuget|
+  cd File.join($dir, "Pollock") do
+    NugetHelper::exec "pack Pollock.csproj"
+  end
+end
+
+task :pollock_json_pack => [:build] do |nuget|
+  cd File.join($dir, "Pollock.Newtonsoft.Json") do
+    NugetHelper::exec "pack Pollock.Newtonsoft.Json.csproj -IncludeReferencedProjects"
+  end
+end
+
+task :pollock_push do |nuget|
+  cd File.join($dir, "Pollock") do
+    latest = NugetHelper.last_version(Dir.glob("Pollock.*.nupkg"))
+    NugetHelper::exec("push #{latest}")
+  end
+end
+
+task :pollock_json_push do |nuget|
+  cd File.join($dir, "Pollock.Newtonsoft.Json") do
+    latest = NugetHelper.last_version(Dir.glob("Pollock.Newtonsoft.Json.*.nupkg"))
+    NugetHelper::exec("push #{latest}")
+  end
+end
